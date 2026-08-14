e5c7038d49848ec477e68a31b2c3feadc6a5cf8b
Ayushi Singh <ayu.s28.ayushi@gmail.com>
2026-08-14 18:27:54 +0530

    added config for openapi

---
+++ src/main/java/com/triage/dera/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.triage.dera.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "DeraAPI", version = "1.0", description = "Documentation for Dera Backend APIs")
+)
+@SecurityScheme(
+        name = "Bearer Authentication",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApiConfig {
+}
